96e59772153b814fd3a789f5def25ef2bff5ceb7
Sebastian Reimers <hallo@studio-link.de>
2017-10-22 13:21:29 +0200

    Replace default theme and layout

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ JNIEXPORT jstring JNICALL
 Java_link_studio_app_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "1299abcdef";
     return env->NewStringUTF(hello.c_str());
 }
