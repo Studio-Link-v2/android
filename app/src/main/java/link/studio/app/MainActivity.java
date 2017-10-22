615ae28ec6a0d3ee48a808841e79da66e1927376
Sebastian Reimers <hallo@studio-link.de>
2017-10-22 14:10:58 +0200

    integrate basic baresip PoC

--- app/src/main/java/link/studio/app/MainActivity.java
+++ app/src/main/java/link/studio/app/MainActivity.java
@@ -2,14 +2,17 @@ package link.studio.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.*;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
+    private Boolean running = false;
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("baresip");
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,42 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.studioid);
-        tv.setText(stringFromJNI());
+     //   tv.setText(stringFromJNI());
+
+
+        if (!running) {
+            new Thread(new Runnable() {
+                public void run() {
+                    baresipStart();
+                }
+            }).start();
+            //((Button) v).setText("Running");
+            running = true;
+        }
+        Button quitButton = (Button)findViewById(R.id.quit);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (running) {
+                    baresipStop();
+                    running = false;
+                }
+/*                finish(); */
+                System.exit(0);
+            }
+        });
+
+
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
+
+    public native void baresipStart();
+    public native void baresipStop();
+
+
 }
